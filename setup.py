ea0edc491dc945cddce2ae86c8d3ce8dd6e27099
cblancom <cristian.blanco@utp.edu.co>
2022-11-18 16:03:46 -0500

    setup

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author_email="<cristian.blanco@utp.edu.co>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    packages=find_packages(),
+    packages=['network'],
     install_requires=['cycler', 'et-xmlfile', 'fonttools', 
                         'gekko', 'kiwisolver' ,'matplotlib', 
                         'networkx', 'numpy', 'openpyxl', 
