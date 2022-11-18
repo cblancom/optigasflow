e7ae155477ade1a8c5b82b517c72d3ae9c4ce3c6
cblancom <cristian.blanco@utp.edu.co>
2022-11-18 15:54:35 -0500

    setup

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
 from setuptools import setup, find_packages
+import codecs
+import os
 
-setup(name="Network",
-    version="0.1", 
-    description="Python script for compute gas natural flow", 
-    author="cblancom",  
-    author_email='cristian.blanco@utp.edu.co', 
-    license="GPL",  
-    url="https://github.com/cblancom/natural_gas_project",  
+VERSION = '0.0.1'
+DESCRIPTION = 'compute gas natural flow'
+
+# Setting up
+setup(
+    name="Network",
+    version=VERSION,
+    author="cblancom",
+    author_email="<cristian.blanco@utp.edu.co>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=[i.strip() for i in open("requirements.txt").readlines()],
+    install_requires=['cycler', 'et-xmlfile', 'fonttools', 
+                        'gekko', 'kiwisolver' ,'matplotlib', 
+                        'networkx', 'numpy', 'openpyxl', 
+                        'packaging', 'pandas', 'Pillow==9.3.0', 
+                        'pyparsing','python-dateutil', 'pytz', 'scipy' , 'six'],
+    keywords=['python', 'natural gas'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3",
+    ]
 )
