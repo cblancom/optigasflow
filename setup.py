938c988d4a0c806e5f3fbbd80521cb0032fcab1e
cblancom <cristian.blanco@utp.edu.co>
2022-11-18 12:04:30 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name="Network",
+    version="0.1", 
+    description="Python script for compute gas natural flow", 
+    author="cblancom",  
+    author_email='cristian.blanco   utp.edu.co', 
+    license="GPL",  
+    url="https://github.com/cblancom/natural_gas_project",  
+    packages=find_packages(),
+    install_requires=[i.strip() for i in open("requirements.txt").readlines()],
+)
