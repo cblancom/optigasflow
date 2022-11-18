7c8d9951bdb7099674fd430dad1a3e421aac9635
cblancom <cristian.blanco@utp.edu.co>
2022-11-18 15:23:01 -0500

    setup

--- network/network.py
+++ network/network.py
@@ -1,5 +1,9 @@
-# network = pd.ExcelFile(path)
-
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+import pandas as pd
+from gekko import GEKKO
+from scipy.sparse import coo_matrix, eye, hstack
 
 class Network:
 
