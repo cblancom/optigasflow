79fb29e26c2c066d9bfd34f0dc6788ec1fe6f214
cblancom <cristian.blanco@utp.edu.co>
2022-11-18 11:09:56 -0500

    create a network folder

--- test.py
+++ test.py
@@ -1,18 +1,8 @@
-# import pandas as pd
-# from network import *
+import pandas as pd
+from network import *
 
-# path_file = "/home/cristianblanco/projects/natural_gas_project/database/ng_case8.xlsx"
-# data = pd.ExcelFile(path_file)
-# p = Network(data) 
+path_file = "/home/cristianblanco/projects/natural_gas_project/database/ng_case8.xlsx"
+data = pd.ExcelFile(path_file)
+p = Network(data) 
 
-# print(p.get_values())
-
-
-from setuptools import setup, find_packages
-
-
-setup(
-    name="foo",
-    version="1.0",
-    packages=find_packages(),
-)
+print(p.get_values())
